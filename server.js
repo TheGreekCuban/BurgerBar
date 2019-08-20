4e3770f3c6da67a7dff5cff3c8c82ac28eb04dc2
TheGreekCuban <theodorealexander92@gmaill.com>
2019-08-20 14:02:20 -0400

    node mod

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+//Save the initialization of express to the app variable
+const app = express()
+
+//Create a variable that willl be the PORT
+const PORT = process.env.PORT || 3030
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER - The below points our server to a series of "route" files.
+//require("./routing/apiRoutes")(app);
+//require("./routing/htmlRoutes")(app);
+
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+  console.log("App listening on http://localhost:" + PORT);
+});
