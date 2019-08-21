24d4e85d9c28efa4d7154777d1e5e9a3d77dd198
TheGreekCuban <theodorealexander92@gmaill.com>
2019-08-21 16:10:47 -0400

    finished controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -4,8 +4,8 @@ const router = express.Router()
 
 const burger = require("../models/burger.js")
 
-router.get("/", function(request, response) {
-    burger.selectAll(data => {
+router.get("/", function (request, response) {
+    burger.selectAll(function (data) {
         let burgerObject = {
             burgers: data
         }
@@ -13,3 +13,27 @@ router.get("/", function(request, response) {
         response.render('index', burgerObject)
     })
 })
+
+router.post("/api/burgers", (request, response) => {
+    burger.insertOne("burger_name", [req.body.burger_name], result => {
+        console.log(`api/burgers result: ${result}`)
+        response.json({
+            id: result
+        })
+    })
+})
+
+router.put("/api/burgers/:id", (request, response) => {
+    let condition = `id = ${req.params.id}`
+    console.log(`condidition: ${condition}`)
+    burger.updateOne({
+        devoured: request.body.devoured
+    }, condition, result => {
+        if (result.changedRows === 0) {
+            return response.status(404).end()
+        }
+        response.status(200).end()
+    })
+})
+
+module.exports = router
